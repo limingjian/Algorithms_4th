60afb349980c2a31201f3144d52da945a9fa2a4f
limingjian <limingjianbd@gmail.com>
2017-09-07 10:06:57 +0800

    First commit

---
+++ src/test.java
@@ -0,0 +1,13 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StdOut.println("Hello!");
+		int a = StdIn.readInt();
+		System.out.println("a is "+a);
+	}
+
+}
